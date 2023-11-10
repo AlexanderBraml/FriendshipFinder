b70fa8eaa11266974c2d24dee8793be9813143ff
truePhilipp <philipp@mayr.software>
2023-11-10 15:06:09 +0100

    base iOS project added

---
+++ FriendshipFinderiOS/FriendshipFinderiOS/FriendshipFinderiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendshipFinderiOSApp.swift
+//  FriendshipFinderiOS
+//
+//  Created by Philipp Mayr on 10.11.23.
+//
+
+import SwiftUI
+
+@main
+struct FriendshipFinderiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
