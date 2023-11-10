9aa7a75d83a9cb46cdca9df97178e612a7066e9d
Alexander Braml <braml@fim.uni-passau.de>
2023-11-10 16:04:46 +0100

    Implement android device exploration

--- FriendshipFinderAndroid/app/src/main/java/de/hackathon/friendshipfinderandroid/MainActivity.java
+++ FriendshipFinderAndroid/app/src/main/java/de/hackathon/friendshipfinderandroid/MainActivity.java
@@ -1,18 +1,102 @@
 package de.hackathon.friendshipfinderandroid;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayAdapter<String> devicesArrayAdapter;
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView text = findViewById(R.id.mainTextView);
-        text.setText("Hi Philipp!");
+        String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.BLUETOOTH,
+                android.Manifest.permission.BLUETOOTH_ADMIN,
+                android.Manifest.permission.BLUETOOTH_CONNECT,
+                android.Manifest.permission.BLUETOOTH_SCAN};
+        ActivityCompat.requestPermissions(this, permissions, 12453);
+
+        // Überprüfe, ob Bluetooth auf dem Gerät verfügbar ist
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Bluetooth wird auf diesem Gerät nicht unterstützt
+            // Füge hier eine geeignete Behandlung hinzu
+            return;
+        }
+
+        // Überprüfe, ob Bluetooth aktiviert ist
+        if (!bluetoothAdapter.isEnabled()) {
+            // Bluetooth ist deaktiviert, fordere den Benutzer auf, es zu aktivieren
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+
+        // Initialisiere die ListView für die Bluetooth-Geräte
+        ListView devicesListView = findViewById(R.id.devicesListView);
+        devicesArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        devicesListView.setAdapter(devicesArrayAdapter);
+
+        // Zeige bereits gepaarte Geräte an
+        showPairedDevices();
+
+        // Registriere den BroadcastReceiver für Geräteentdeckung
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+        // Starte die Geräteentdeckung
+        bluetoothAdapter.startDiscovery();
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Beende die Geräteentdeckung und unregister den BroadcastReceiver
+        if (bluetoothAdapter != null) {
+            bluetoothAdapter.cancelDiscovery();
+        }
+        unregisterReceiver(receiver);
+    }
+
+    private void showPairedDevices() {
+        // Zeige bereits gepaarte Geräte an
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                devicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    }
+
+    // BroadcastReceiver für die Geräteentdeckung
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Ein neues Bluetooth-Gerät wurde gefunden
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                devicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
 }
